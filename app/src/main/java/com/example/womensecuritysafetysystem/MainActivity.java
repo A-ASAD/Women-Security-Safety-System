4a049159bb0316bca4406380b6434441e3e289bb
Abdurrahman Asad <bsef17m005@pucit.edu.pk>
2020-07-04 12:07:55 +0500

    Created basic layout and splash screen...

---
+++ app/src/main/java/com/example/womensecuritysafetysystem/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.womensecuritysafetysystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        getWindow().setNavigationBarColor(getResources().getColor(R.color.colorPrimary));
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(MainActivity.this, intro_splash.class);
+                startActivity(splash);
+                finish();
+            }
+        },2000);
+    }
+}
---
+++ app/src/main/java/com/example/womensecuritysafetysystem/intro_splash.java
@@ -0,0 +1,17 @@
+package com.example.womensecuritysafetysystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class intro_splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_splash);
+
+    }
+}
