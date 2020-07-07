4720e1b71f94ce15f37c8710033f92b097326a14
hamza-hameed <bsef17m052@gmail.com>
2020-07-07 10:22:11 -0700

    add-guardian page added!

--- app/src/main/java/com/example/womensecuritysafetysystem/add_guardian.java
+++ app/src/main/java/com/example/womensecuritysafetysystem/add_guardian.java
@@ -6,21 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-public class home extends AppCompatActivity {
+public class add_guardian extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_add_guardian);
         getSupportActionBar().hide();
         getWindow().setNavigationBarColor(getResources().getColor(R.color.colorPrimary));
-        final TextView add_guardian = (TextView)findViewById(R.id.button3);
-        add_guardian.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent add_guardian = new Intent(home.this, add_guardian.class);
-                startActivity(add_guardian);
-            }
-        });
 
     }
 }
